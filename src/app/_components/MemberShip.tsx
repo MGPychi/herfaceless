154ac8578f35679a265c022c597096718c1af7a7
chihab <chihab.mg.me@gmail.com>
2024-11-20 23:13:10 +0100

    changed the favicon

--- src/app/_components/MemberShip.tsx
+++ src/app/_components/MemberShip.tsx
@@ -1,26 +1,29 @@
 import { Button } from "@/components/ui/button";
-import { Sparkles } from "lucide-react";
+// import { Sparkles } from "lucide-react";
 import * as motion from "framer-motion/m";
+import CoverImage from "../../../public/cover.webp"
+import Image from "next/image";
 
 export default function MemberShip() {
 	return (
-		<section className="lg:min-h-screen bg-ground  flex items-center justify-center p-6">
+		<section className="lg:min-h-[70vh] bg-ground  flex items-center justify-center p-6">
 			<div className="max-w-7xl w-full mx-auto grid lg:grid-cols-2 gap-12 lg:gap-20 items-center">
 				<motion.div
 					initial={{ opacity: 0, x: -50 }}
 					whileInView={{ opacity: 1, x: 0 }}
 					viewport={{ once: true }}
 					transition={{ duration: 0.5 }}
-					className="relative"
+					className="relative  "
 				>
-					<div className="aspect-[3/4] hidden lg:block h-[60vh] mx-auto  lg:h-auto relative rounded-[2.5rem] border-8 border-black bg-[#e5d0ba] overflow-hidden shadow-xl">
+					<Image className="shadow" src={CoverImage} height={850} width={450}  alt="book cover image" />
+					{/* <div className="aspect-[3/4] hidden lg:block h-[60vh] mx-auto  lg:h-auto relative rounded-[2.5rem] border-8 border-black bg-[#e5d0ba] overflow-hidden shadow-xl">
 						<div className="absolute inset-0 flex flex-col items-center justify-center p-8 text-center">
 							<div className="absolute top-1/2 right-0 w-1/2 h-full bg-black transform translate-x-1/4 rotate-12" />
 							<div className="absolute bottom-1/4 right-1/4 w-1/2 h-1/3 bg-[#4a3428] transform -rotate-45" />
 							<Sparkles className="absolute top-1/4 right-1/4 w-6 h-6" />
 							<Sparkles className="absolute bottom-1/3 right-1/3 w-4 h-4" />
 						</div>
-					</div>
+					</div> */}
 				</motion.div>
 
 				<motion.div
