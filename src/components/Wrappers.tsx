6259839ae588a4357a1b50c50c3e82d796e7cf7a
chihab <chihab.mg.me@gmail.com>
2024-11-22 15:47:07 +0100

    added posthog analytics

--- src/components/Wrappers.tsx
+++ src/components/Wrappers.tsx
@@ -2,10 +2,17 @@
 import React, { ReactNode } from "react";
 import { LazyMotion, domAnimation } from "framer-motion";
 import { Toaster } from "react-hot-toast";
+import { PostHogProvider } from "posthog-js/react";
+
+
 const Wrappers = ({ children }: { children: ReactNode }) => {
 	return (
 		<>
-			<LazyMotion features={domAnimation}>{children}</LazyMotion>
+			<LazyMotion features={domAnimation}>
+				<PostHogProvider>
+				{children}
+				</PostHogProvider>
+				</LazyMotion>
 			<Toaster position="bottom-right" />
 		</>
 	);
